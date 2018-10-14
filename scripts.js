3a2212f46a12ce30da299bd64f69790627883725
Imri Naim <imri294@gmail.com>
2018-10-14 22:00:08 +0300

    without the m

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/*
+var person = {
+  firstname: 'default',
+  lastname: 'default',
+  greet: function () {
+    return `Hi, ${this.firstname}`
+  }
+};
+
+var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Amzaleg';
+console.log(john);
+*/
+
+/*======================================*/
+
+const formatDate = ({day, month, year}) => {
+  return console.log(`${day}/${month}/${year}`);
+};
+
+formatDate({day: 1, month: 2, year: 2018});
